4c5c2c102c834df7ac7eddfd0b65cdd3443fbb32
Isaac Pateau <isaacpateau05@gmail.com>
2015-04-19 14:59:04 -0400

    Quantaboot: Mount /system before renaming the Power HAL elements

--- tmp/anykernel/quantaboot.sh
+++ tmp/anykernel/quantaboot.sh
@@ -3,11 +3,15 @@
 # Initally based from Francisco Franco's fkbootscript.sh
 #
 
-# disable the PowerHAL since there is a kernel-side touch boost implemented
- [ -e /system/lib/hw/power.msm8960.so.bak ] || cp /system/lib/hw/power.msm8960.so /system/lib/hw/power.msm8960.so.bak;
- [ -e /system/lib/hw/power.msm8960.so ] && rm -f /system/lib/hw/power.msm8960.so;
- [ -e /system/bin/mpdecision ] && mv /system/bin/mpdecision /system/bin/mpdecision.bak
+mount -o rw,remount /system;
+
+# Disable the default Power HAL by renaming it's elements
+ [ -e /system/lib/hw/power.msm8960.so ] && mv /system/lib/hw/power.msm8960.so /system/lib/hw/power.msm8960.so.bak;
+ [ -e /system/bin/mpdecision ] && mv /system/bin/mpdecision /system/bin/mpdecision.bak;
+ [ -e /system/lib/hw/power.mako.so ] && mv /system/lib/hw/power.mako.so /system/lib/hw/power.mako.so.bak;
 
 # enable custom configuration for dt2w
- [ -x /sbin/dt2wconf.sh ] && /system/bin/sh /sbin/dt2wconf.sh
- [ -x /system/etc/init.dt2w.sh ] && /system/bin/sh /system/etc/init.dt2w.sh
+ [ -x /sbin/dt2wconf.sh ] && /system/bin/sh /sbin/dt2wconf.sh;
+ [ -x /system/etc/init.dt2w.sh ] && /system/bin/sh /system/etc/init.dt2w.sh;
+
+mount -o ro,remount /system;
