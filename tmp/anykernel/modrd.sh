75b72bfd35d4349c4d85e987f6ff27f4def857c7
Isaac Pateau <isaacpateau05@gmail.com>
2015-04-19 18:59:54 -0400

    modrd.sh: Add more ramdisk values from Franco
    - Block subsystem tweaks
    - Change the load_threshold to 85
    - Change some permission related stuff
    - Minor cleanup in code

--- tmp/anykernel/modrd.sh
+++ tmp/anykernel/modrd.sh
@@ -5,7 +5,7 @@
 #
 
 
-# Check to see if there's any occurence of any of franco's tweaks in the ramdisk
+# Check to see if there's any occurence of franco's tweaks in the ramdisk
 francotweaks=`grep -c "import init.performance_profiles.rc" init.mako.rc`
 
 # Copy quanta boot script
@@ -23,9 +23,14 @@ fi
 
 # Modifications to init.mako.rc
 if [ $francotweaks -eq 0 ] ; then
+sed '/on boot/ a\    write /sys/block/mmcblk0/queue/nomerges 1' -i init.mako.rc
+sed '/queue\/nomerges 1/ a\    write /sys/block/mmcblk0/queue/rq_affinity 2' -i init.mako.rc
+sed '/queue\/rq_affinity 2/ a\    write /sys/block/mmcblk0/queue/add_random 0' -i init.mako.rc
+sed '/queue\/add_random 0/ a\    write /sys/block/mmcblk0/queue/read_ahead_kb 1024' -i init.mako.rc
+sed '/queue\/read_ahead_kb 1024/ a\\' -i init.mako.rc
 sed '/scaling_governor/ s/ondemand/conservative/g' -i init.mako.rc
-sed '/ondemand/d' -i init.mako.rc
-sed '/cpu.notify_on_migrate /s/1/0/g' -i init.mako.rc
+sed '/ondemand/ d' -i init.mako.rc
+sed '/cpu.notify_on_migrate/ s/1/0/g' -i init.mako.rc
 sed '/group radio system/ a\    disabled' -i init.mako.rc
 sed '/group root system/ a\    disabled' -i init.mako.rc
 sed '/cpu0\/cpufreq\/scaling_governor "conservative"/ i\    write /sys/devices/system/cpu/cpu1/online 1 ' -i init.mako.rc
@@ -35,12 +40,10 @@ fi
 
 # Modidfications to init.rc
 if [ $francotweaks -eq 0 ] ; then
-sed '/sys\/devices/ s/0660/0664/g' -i init.rc
-sed '/chmod 0664 \/sys\/devices\/system\/cpu\/cpu0\/cpufreq\/scaling_max_freq/ a\    chown system system /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq' -i init.rc
-sed '/chown system system \/sys\/devices\/system\/cpu\/cpu0\/cpufreq\/scaling_min_freq/ a\    chmod 0664 /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq' -i init.rc
-sed '/chmod 0664 \/sys\/devices\/system\/cpu\/cpu0\/cpufreq\/scaling_min_freq/ a\    chown system system /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor' -i init.rc
-sed '/chown system system \/sys\/devices\/system\/cpu\/cpu0\/cpufreq\/scaling_governor a\    chmod 0664 /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor' -i init.rc
-sed '/seclabel u:r:install_recovery:s0/d' -i init.rc
+sed '/chmod 0660 \/sys\/devices\/system\/cpu\/cpu0\/cpufreq\/scaling_max_freq/ a\    chown system system /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq' -i init.rc
+sed '/chown system system \/sys\/devices\/system\/cpu\/cpu0\/cpufreq\/scaling_min_freq/ a\    chmod 0660 /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq' -i init.rc
+sed '/chmod 0660 \/sys\/devices\/system\/cpu\/cpu0\/cpufreq\/scaling_min_freq/ a\    chown system system /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor' -i init.rc
+sed '/chown system system \/sys\/devices\/system\/cpu\/cpu0\/cpufreq\/scaling_governor a\    chmod 0660 /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor' -i init.rc
 fi
 
 # Applying franco's stuff after boot
@@ -59,6 +62,6 @@ on property:sys.boot_completed=1
     write /sys/devices/system/cpu/cpufreq/conservative/up_threshold 95
     write /sys/devices/system/cpu/cpufreq/conservative/freq_step 10
     write /sys/devices/system/cpu/cpufreq/conservative/down_threshold 40
-    write /sys/class/misc/mako_hotplug_control/load_threshold 65
+    write /sys/class/misc/mako_hotplug_control/load_threshold 85
     write /sys/class/misc/mako_hotplug_control/high_load_counter 5 " >> init.mako.rc
 fi
