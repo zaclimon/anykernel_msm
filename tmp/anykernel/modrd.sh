b7410f0ef66fab7b721e5720d7774a3df5efd79b
Isaac Pateau <isaacpateau05@gmail.com>
2015-12-27 16:34:15 -0500

    Modrd.sh: Restore init.$DEVICE.rc to init.flo.rc
    At build process, init.deb.rc gets renamed to init.flo.rc so it's not
    necessary to find a way to rename it. The script would have found init.flo.rc
    anyways.

--- tmp/anykernel/modrd.sh
+++ tmp/anykernel/modrd.sh
@@ -5,35 +5,32 @@
 #
 
 
-# Determine the device to know whether we're using a Flo or a Deb
-DEVICE=`find fstab.* | cut -d . -f2`
-
 # Check to see if there's any occurence of performance profile script in the ramdisk
-francotweaks=`grep -c "import init.performance_profiles.rc" init.$DEVICE.rc`
+francotweaks=`grep -c "import init.performance_profiles.rc" init.flo.rc`
 
 # Apply performance settings stuff
 if [ $francotweaks -eq 0 ] ; then
-sed '/import init.flo.diag.rc/a \import init.performance_profiles.rc' -i init.$DEVICE.rc
+sed '/import init.flo.diag.rc/a \import init.performance_profiles.rc' -i init.flo.rc
 cp ../init.performance_profiles.rc ./
 chmod 0755 init.performance_profiles.rc
 fi
 
 # Modifications to init.flo.rc/init.deb.rc
 if [ $francotweaks -eq 0 ] ; then
-sed '/on boot/ a\    write /sys/block/mmcblk0/queue/nomerges 1' -i init.$DEVICE.rc
-sed '/queue\/nomerges 1/ a\    write /sys/block/mmcblk0/queue/rq_affinity 2' -i init.$DEVICE.rc
-sed '/queue\/rq_affinity 2/ a\    write /sys/block/mmcblk0/queue/add_random 0' -i init.$DEVICE.rc
-sed '/queue\/add_random 0/ a\    write /sys/block/mmcblk0/queue/read_ahead_kb 1024' -i init.$DEVICE.rc
-sed '/queue\/read_ahead_kb 1024/ a\\' -i init.$DEVICE.rc
-sed '/scaling_governor/ s/ondemand/conservative/g' -i init.$DEVICE.rc
-sed '/ondemand/ d' -i init.$DEVICE.rc
-sed '/cpu.notify_on_migrate/ s/1/0/g' -i init.$DEVICE.rc
-sed '/group radio system/ a\    disabled' -i init.$DEVICE.rc
-sed '/group root system/ a\    disabled' -i init.$DEVICE.rc
-sed '/online 1/ d' -i init.$DEVICE.rc
-sed '/cpu0\/cpufreq\/scaling_governor "conservative"/ i\    write /sys/devices/system/cpu/cpu1/online 1 ' -i init.$DEVICE.rc
-sed '/cpu1\/online 1/ a\    write /sys/devices/system/cpu/cpu2/online 1' -i init.$DEVICE.rc
-sed '/cpu2\/online 1/ a\    write /sys/devices/system/cpu/cpu3/online 1' -i init.$DEVICE.rc
+sed '/on boot/ a\    write /sys/block/mmcblk0/queue/nomerges 1' -i init.flo.rc
+sed '/queue\/nomerges 1/ a\    write /sys/block/mmcblk0/queue/rq_affinity 2' -i init.flo.rc
+sed '/queue\/rq_affinity 2/ a\    write /sys/block/mmcblk0/queue/add_random 0' -i init.flo.rc
+sed '/queue\/add_random 0/ a\    write /sys/block/mmcblk0/queue/read_ahead_kb 1024' -i init.flo.rc
+sed '/queue\/read_ahead_kb 1024/ a\\' -i init.flo.rc
+sed '/scaling_governor/ s/ondemand/conservative/g' -i init.flo.rc
+sed '/ondemand/ d' -i init.flo.rc
+sed '/cpu.notify_on_migrate/ s/1/0/g' -i init.flo.rc
+sed '/group radio system/ a\    disabled' -i init.flo.rc
+sed '/group root system/ a\    disabled' -i init.flo.rc
+sed '/online 1/ d' -i init.flo.rc
+sed '/cpu0\/cpufreq\/scaling_governor "conservative"/ i\    write /sys/devices/system/cpu/cpu1/online 1 ' -i init.flo.rc
+sed '/cpu1\/online 1/ a\    write /sys/devices/system/cpu/cpu2/online 1' -i init.flo.rc
+sed '/cpu2\/online 1/ a\    write /sys/devices/system/cpu/cpu3/online 1' -i init.flo.rc
 fi
 
 # Modifications to init.rc
@@ -55,5 +52,5 @@ on property:sys.boot_completed=1
     write /sys/devices/system/cpu/cpufreq/conservative/freq_step 10
     write /sys/devices/system/cpu/cpufreq/conservative/down_threshold 40
     write /sys/class/misc/mako_hotplug_control/load_threshold 80
-    write /sys/class/misc/mako_hotplug_control/high_load_counter 5 " >> init.$DEVICE.rc
+    write /sys/class/misc/mako_hotplug_control/high_load_counter 5 " >> init.flo.rc
 fi
