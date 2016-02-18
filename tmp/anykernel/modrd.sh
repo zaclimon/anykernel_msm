d4df1fd3274b98285926a049869f0fd86e5776d5
Isaac Pateau <isaacpateau05@gmail.com>
2016-02-18 18:44:44 -0500

    Reoptimize values for Quanta
    - Revamp modrd.sh by adapting new values to optimize the kernel
    - Adapt for new values from flo's franco.Kernel
    - Adapt init.performance_profiles.rc for the values from modrd.sh
    - Remove quantaboot.sh as it isn't necessary anymore.

--- tmp/anykernel/modrd.sh
+++ tmp/anykernel/modrd.sh
@@ -17,38 +17,60 @@ fi
 
 # Modifications to init.mako.rc
 if [ $francotweaks -eq 0 ] ; then
-sed '/on boot/ a\    write /sys/block/mmcblk0/queue/nomerges 1' -i init.mako.rc
+sed '/scaling_governor/ s/ondemand/interactive/g' -i init.mako.rc
+sed '/ondemand/ d' -i init.mako.rc
+
+sed '/cpu3\/cpufreq\/scaling_min_freq 384000/ a\    write /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq 1242000' -i init.mako.rc
+sed '/cpu0\/cpufreq\/scaling_max_freq 1242000/ a\    write /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq 1242000' -i init.mako.rc
+sed '/cpu1\/cpufreq\/scaling_max_freq 1242000/ a\    write /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq 1242000' -i init.mako.rc
+sed '/cpu2\/cpufreq\/scaling_max_freq 1242000/ a\    write /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq 1242000' -i init.mako.rc
+sed '/# disable diag port/ i\    write /sys/devices/system/cpu/cpu1/online 1' -i init.mako.rc
+sed '/# disable diag port/ i\    write /sys/devices/system/cpu/cpu2/online 1' -i init.mako.rc
+sed '/# disable diag port/ i\    write /sys/devices/system/cpu/cpu3/online 1' -i init.mako.rc
+sed '/# disable diag port/ i\\' -i init.mako.rc
+sed '/# disable diag port/ i\    # Interactive' -i init.mako.rc
+sed '/# disable diag port/ i\\' -i init.mako.rc
+
+sed '/# Interactive/ a\    restorecon_recursive /sys/devices/system/cpu/cpufreq/interactive' -i init.mako.rc
+sed '/restorecon_recursive \/sys\/devices\/system\/cpu\/cpufreq\/interactive/ a\    write /sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay "20000 918000:60000 1242000:150000"' -i init.mako.rc
+sed '/interactive\/above_hispeed_delay "20000 918000:60000 1242000:150000"/ a\    write /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load 99' -i init.mako.rc
+sed '/interactive\/go_hispeed_load 99/ a\    write /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq 384000' -i init.mako.rc
+sed '/interactive\/hispeed_freq 384000/ a\    write /sys/devices/system/cpu/cpufreq/interactive/io_is_busy 1' -i init.mako.rc
+sed '/interactive\/io_is_busy 1/ a\    write /sys/devices/system/cpu/cpufreq/interactive/target_loads "90 384000:40 918000:80 1242000:95"' -i init.mako.rc
+sed '/interactive\/target_loads "90 384000:40 918000:80 1242000:95"/ a\    write /sys/devices/system/cpu/cpufreq/interactive/min_sample_time 60000' -i init.mako.rc
+sed '/interactive\/min_sample_time 60000/ a\    write /sys/devices/system/cpu/cpufreq/interactive/timer_rate 30000' -i init.mako.rc
+sed '/interactive\/timer_rate 30000/ a\    write /sys/devices/system/cpu/cpufreq/interactive/max_freq_hysteresis 100000' -i init.mako.rc
+sed '/interactive\/max_freq_hysteresis 100000/ a\    write /sys/devices/system/cpu/cpufreq/interactive/timer_slack 80000' -i init.mako.rc
+sed '/interactive\/timer_slack 80000/ a\    # GPU' -i init.mako.rc
+sed '/interactive\/timer_slack 80000/ a\\' -i init.mako.rc
+
+sed '/# GPU/ a\    write /sys/devices/platform/kgsl-3d0.0/kgsl/kgsl-3d0/max_gpuclk 200000000' -i init.mako.rc
+sed '/kgsl-3d0\/max_gpuclk 200000000/ a\    # Cpu-Boost' -i init.mako.rc
+sed '/kgsl-3d0\/max_gpuclk 200000000/ a\\' -i init.mako.rc
+
+sed '/# Cpu-Boost/ a\    write /sys/module/cpu_boost/parameters/boost_ms 20' -i init.mako.rc
+sed '/cpu_boost\/parameters\/boost_ms 20/ a\    write /sys/module/cpu_boost/parameters/input_boost_freq 918000' -i init.mako.rc
+sed '/cpu_boost\/parameters\/input_boost_freq 918000/ a\    write /sys/module/cpu_boost/parameters/input_boost_ms 100' -i init.mako.rc
+sed '/cpu_boost\/parameters\/input_boost_ms 100/ a\    # I/O' -i init.mako.rc
+sed '/cpu_boost\/parameters\/input_boost_ms 100/ a\\' -i init.mako.rc
+
+sed '/# I\/O/ a\    write /sys/block/mmcblk0/queue/nomerges 1' -i init.mako.rc
 sed '/queue\/nomerges 1/ a\    write /sys/block/mmcblk0/queue/rq_affinity 2' -i init.mako.rc
 sed '/queue\/rq_affinity 2/ a\    write /sys/block/mmcblk0/queue/add_random 0' -i init.mako.rc
-sed '/queue\/add_random 0/ a\    write /sys/block/mmcblk0/queue/read_ahead_kb 1024' -i init.mako.rc
-sed '/queue\/read_ahead_kb 1024/ a\\' -i init.mako.rc
-sed '/scaling_governor/ s/ondemand/conservative/g' -i init.mako.rc
-sed '/ondemand/ d' -i init.mako.rc
-sed '/cpu.notify_on_migrate/ s/1/0/g' -i init.mako.rc
+sed '/queue\/add_random 0/ a\    write /sys/block/mmcblk0/bdi/min_ratio 5' -i init.mako.rc
+sed '/bdi\/min_ratio 5/ a\    # KSM' -i init.mako.rc
+sed '/bdi\/min_ratio 5/ a\\' -i init.mako.rc
+
+sed '/# KSM/ a\    write /sys/kernel/mm/ksm/sleep_millisecs 1500' -i init.mako.rc
+sed '/ksm\/sleep_millisecs 1500/ a\    write /sys/kernel/mm/ksm/pages_to_scan 256' -i init.mako.rc
+sed '/ksm\/pages_to_scan 256/ a\    write /sys/kernel/mm/ksm/deferred_timer 1' -i init.mako.rc
+sed '/ksm\/deferred_timer 1/ a\    write /sys/kernel/mm/ksm/run 1' -i init.mako.rc
+
 sed '/group radio system/ a\    disabled' -i init.mako.rc
 sed '/group root system/ a\    disabled' -i init.mako.rc
-sed '/cpu0\/cpufreq\/scaling_governor "conservative"/ i\    write /sys/devices/system/cpu/cpu1/online 1 ' -i init.mako.rc
-sed '/cpu1\/online 1/ a\    write /sys/devices/system/cpu/cpu2/online 1' -i init.mako.rc
-sed '/cpu2\/online 1/ a\    write /sys/devices/system/cpu/cpu3/online 1' -i init.mako.rc
 fi
 
-# Modidfications to init.rc
+# Modifications to init.rc
 if [ $francotweaks -eq 0 ] ; then
-sed '/chmod 0660 \/sys\/devices\/system\/cpu\/cpu0\/cpufreq\/scaling_max_freq/ a\    chown system system /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq' -i init.rc
-sed '/chown system system \/sys\/devices\/system\/cpu\/cpu0\/cpufreq\/scaling_min_freq/ a\    chmod 0660 /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq' -i init.rc
-sed '/chmod 0660 \/sys\/devices\/system\/cpu\/cpu0\/cpufreq\/scaling_min_freq/ a\    chown system system /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor' -i init.rc
-sed '/chown system system \/sys\/devices\/system\/cpu\/cpu0\/cpufreq\/scaling_governor a\    chmod 0660 /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor' -i init.rc
-fi
-
-# Applying franco's stuff after boot
-if [ $francotweaks -eq 0 ] ; then
-echo "
-on property:sys.boot_completed=1
-    write /sys/block/mmcblk0/queue/scheduler noop
-    write /sys/devices/system/cpu/cpufreq/conservative/input_boost_freq 1512000
-    write /sys/devices/system/cpu/cpufreq/conservative/up_threshold 95
-    write /sys/devices/system/cpu/cpufreq/conservative/freq_step 10
-    write /sys/devices/system/cpu/cpufreq/conservative/down_threshold 40
-    write /sys/class/misc/mako_hotplug_control/load_threshold 85
-    write /sys/class/misc/mako_hotplug_control/high_load_counter 5 " >> init.mako.rc
+sed '/sys\/devices\/system\/cpu\/cpufreq\/interactive/ s/0660/0664/g' -i init.rc
 fi
