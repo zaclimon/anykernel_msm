93ba75b543da1d9b5f738c297804da88a4b9dce1
Isaac Pateau <isaacpateau05@gmail.com>
2016-03-09 13:20:58 -0500

    modrd.sh: Adapt Interactive optimized values for Flo

--- tmp/anykernel/modrd.sh
+++ tmp/anykernel/modrd.sh
@@ -21,20 +21,20 @@ if [ $francotweaks -eq 0 ] ; then
 sed '/scaling_governor/ s/ondemand/interactive/g' -i init.flo.rc
 sed '/ondemand/ d' -i init.flo.rc
 
-sed '/cpu3\/cpufreq\/scaling_min_freq 384000/ a\    write /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq 1512000' -i init.flo.rc
-sed '/cpu0\/cpufreq\/scaling_max_freq 1512000/ a\    write /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq 1512000' -i init.flo.rc
-sed '/cpu1\/cpufreq\/scaling_max_freq 1512000/ a\    write /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq 1512000' -i init.flo.rc
-sed '/cpu2\/cpufreq\/scaling_max_freq 1512000/ a\    write /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq 1512000' -i init.flo.rc
+sed '/cpu3\/cpufreq\/scaling_min_freq 384000/ a\    write /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq 1242000' -i init.flo.rc
+sed '/cpu0\/cpufreq\/scaling_max_freq 1242000/ a\    write /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq 1242000' -i init.flo.rc
+sed '/cpu1\/cpufreq\/scaling_max_freq 1242000/ a\    write /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq 1242000' -i init.flo.rc
+sed '/cpu2\/cpufreq\/scaling_max_freq 1242000/ a\    write /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq 1242000' -i init.flo.rc
 sed '/cpu3\/online 1/ a\    # Interactive' -i init.flo.rc
 sed '/cpu3\/online 1/ a\\' -i init.flo.rc
 
 sed '/# Interactive/ a\    restorecon_recursive /sys/devices/system/cpu/cpufreq/interactive' -i init.flo.rc
-sed '/restorecon_recursive \/sys\/devices\/system\/cpu\/cpufreq\/interactive/ a\    write /sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay "20000"' -i init.flo.rc
-sed '/interactive\/above_hispeed_delay "20000"/ a\    write /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load 95' -i init.flo.rc
-sed '/interactive\/go_hispeed_load 95/ a\    write /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq 1026000' -i init.flo.rc
-sed '/interactive\/hispeed_freq 1026000/ a\    write /sys/devices/system/cpu/cpufreq/interactive/io_is_busy 1' -i init.flo.rc
-sed '/interactive\/io_is_busy 1/ a\    write /sys/devices/system/cpu/cpufreq/interactive/target_loads "85 810000:90 1350000:99"' -i init.flo.rc
-sed '/interactive\/target_loads "85 810000:90 1350000:99"/ a\    write /sys/devices/system/cpu/cpufreq/interactive/min_sample_time 60000' -i init.flo.rc
+sed '/restorecon_recursive \/sys\/devices\/system\/cpu\/cpufreq\/interactive/ a\    write /sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay "20000 1026000:60000 1242000:150000"' -i init.flo.rc
+sed '/interactive\/above_hispeed_delay "20000 1026000:60000 1242000:150000"/ a\    write /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load 99' -i init.flo.rc
+sed '/interactive\/go_hispeed_load 99/ a\    write /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq 384000' -i init.flo.rc
+sed '/interactive\/hispeed_freq 384000/ a\    write /sys/devices/system/cpu/cpufreq/interactive/io_is_busy 1' -i init.flo.rc
+sed '/interactive\/io_is_busy 1/ a\    write /sys/devices/system/cpu/cpufreq/interactive/target_loads "90 384000:40 1026000:80 1242000:95"' -i init.flo.rc
+sed '/interactive\/target_loads "90 384000:40 1026000:80 1242000:95"/ a\    write /sys/devices/system/cpu/cpufreq/interactive/min_sample_time 60000' -i init.flo.rc
 sed '/interactive\/min_sample_time 60000/ a\    write /sys/devices/system/cpu/cpufreq/interactive/timer_rate 30000' -i init.flo.rc
 sed '/interactive\/timer_rate 30000/ a\    write /sys/devices/system/cpu/cpufreq/interactive/max_freq_hysteresis 100000' -i init.flo.rc
 sed '/interactive\/max_freq_hysteresis 100000/ a\    write /sys/devices/system/cpu/cpufreq/interactive/timer_slack 30000' -i init.flo.rc
@@ -46,10 +46,10 @@ sed '/cpu.notify_on_migrate/ a\    # Cpu-Boost' -i init.flo.rc
 sed '/cpu.notify_on_migrate/ a\\' -i init.flo.rc
 
 sed '/# Cpu-Boost/ a\    write /sys/module/cpu_boost/parameters/boost_ms 20' -i init.flo.rc
-sed '/cpu_boost\/parameters\/boost_ms 20/ a\    write /sys/module/cpu_boost/parameters/input_boost_freq 1512000' -i init.flo.rc
-sed '/cpu_boost\/parameters\/input_boost_freq 1512000/ a\    write /sys/module/cpu_boost/parameters/input_boost_ms 250' -i init.flo.rc
-sed '/cpu_boost\/parameters\/input_boost_ms 250/ a\    # I/O' -i init.flo.rc
-sed '/cpu_boost\/parameters\/input_boost_ms 250/ a\\' -i init.flo.rc
+sed '/cpu_boost\/parameters\/boost_ms 20/ a\    write /sys/module/cpu_boost/parameters/input_boost_freq 1026000' -i init.flo.rc
+sed '/cpu_boost\/parameters\/input_boost_freq 1026000/ a\    write /sys/module/cpu_boost/parameters/input_boost_ms 100' -i init.flo.rc
+sed '/cpu_boost\/parameters\/input_boost_ms 100/ a\    # I/O' -i init.flo.rc
+sed '/cpu_boost\/parameters\/input_boost_ms 100/ a\\' -i init.flo.rc
 
 sed '/# I\/O/ a\    write /sys/block/mmcblk0/queue/nomerges 1' -i init.flo.rc
 sed '/queue\/nomerges 1/ a\    write /sys/block/mmcblk0/queue/rq_affinity 2' -i init.flo.rc
