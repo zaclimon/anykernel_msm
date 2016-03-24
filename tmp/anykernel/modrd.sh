11725738bac0e4b3b0a609503400403c1c36b0e3
Isaac Pateau <isaacpateau05@gmail.com>
2016-03-24 18:20:45 -0400

    Balance CPU utilization
    With the coming of Cpu-Boost, all 4 cores of the Snapdragon S4 Pro were
    activated. This SOC isn't configured like it's newer counterparts and can't
    be used as such. (Big.Little architecture)
    
    Disable by default the 3rd and the 4th core and compensate the possible
    performance loss by adjusting the 918Mhz to 1026Mhz. Apply the same thing
    to the performances profiles as well.

--- tmp/anykernel/modrd.sh
+++ tmp/anykernel/modrd.sh
@@ -20,24 +20,24 @@ if [ $francotweaks -eq 0 ] ; then
 sed '/scaling_governor/ s/ondemand/interactive/g' -i init.mako.rc
 sed '/ondemand/ d' -i init.mako.rc
 
-sed '/cpu3\/cpufreq\/scaling_min_freq 384000/ a\    write /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq 1242000' -i init.mako.rc
-sed '/cpu0\/cpufreq\/scaling_max_freq 1242000/ a\    write /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq 1242000' -i init.mako.rc
-sed '/cpu1\/cpufreq\/scaling_max_freq 1242000/ a\    write /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq 1242000' -i init.mako.rc
-sed '/cpu2\/cpufreq\/scaling_max_freq 1242000/ a\    write /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq 1242000' -i init.mako.rc
+sed '/cpu3\/cpufreq\/scaling_min_freq 384000/ a\    write /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq 1512000' -i init.mako.rc
+sed '/cpu0\/cpufreq\/scaling_max_freq 1512000/ a\    write /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq 1512000' -i init.mako.rc
+sed '/cpu1\/cpufreq\/scaling_max_freq 1512000/ a\    write /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq 1512000' -i init.mako.rc
+sed '/cpu2\/cpufreq\/scaling_max_freq 1512000/ a\    write /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq 1512000' -i init.mako.rc
 sed '/# disable diag port/ i\    write /sys/devices/system/cpu/cpu1/online 1' -i init.mako.rc
-sed '/# disable diag port/ i\    write /sys/devices/system/cpu/cpu2/online 1' -i init.mako.rc
-sed '/# disable diag port/ i\    write /sys/devices/system/cpu/cpu3/online 1' -i init.mako.rc
+sed '/# disable diag port/ i\    write /sys/devices/system/cpu/cpu2/online 0' -i init.mako.rc
+sed '/# disable diag port/ i\    write /sys/devices/system/cpu/cpu3/online 0' -i init.mako.rc
 sed '/# disable diag port/ i\\' -i init.mako.rc
 sed '/# disable diag port/ i\    # Interactive' -i init.mako.rc
 sed '/# disable diag port/ i\\' -i init.mako.rc
 
 sed '/# Interactive/ a\    restorecon_recursive /sys/devices/system/cpu/cpufreq/interactive' -i init.mako.rc
-sed '/restorecon_recursive \/sys\/devices\/system\/cpu\/cpufreq\/interactive/ a\    write /sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay "20000 918000:60000 1242000:150000"' -i init.mako.rc
-sed '/interactive\/above_hispeed_delay "20000 918000:60000 1242000:150000"/ a\    write /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load 99' -i init.mako.rc
+sed '/restorecon_recursive \/sys\/devices\/system\/cpu\/cpufreq\/interactive/ a\    write /sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay "20000 1026000:60000 1242000:150000"' -i init.mako.rc
+sed '/interactive\/above_hispeed_delay "20000 1026000:60000 1242000:150000"/ a\    write /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load 99' -i init.mako.rc
 sed '/interactive\/go_hispeed_load 99/ a\    write /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq 384000' -i init.mako.rc
 sed '/interactive\/hispeed_freq 384000/ a\    write /sys/devices/system/cpu/cpufreq/interactive/io_is_busy 1' -i init.mako.rc
-sed '/interactive\/io_is_busy 1/ a\    write /sys/devices/system/cpu/cpufreq/interactive/target_loads "90 384000:40 918000:80 1242000:95"' -i init.mako.rc
-sed '/interactive\/target_loads "90 384000:40 918000:80 1242000:95"/ a\    write /sys/devices/system/cpu/cpufreq/interactive/min_sample_time 60000' -i init.mako.rc
+sed '/interactive\/io_is_busy 1/ a\    write /sys/devices/system/cpu/cpufreq/interactive/target_loads "90 384000:40 1026000:80 1242000:95"' -i init.mako.rc
+sed '/interactive\/target_loads "90 384000:40 1026000:80 1242000:95"/ a\    write /sys/devices/system/cpu/cpufreq/interactive/min_sample_time 60000' -i init.mako.rc
 sed '/interactive\/min_sample_time 60000/ a\    write /sys/devices/system/cpu/cpufreq/interactive/timer_rate 30000' -i init.mako.rc
 sed '/interactive\/timer_rate 30000/ a\    write /sys/devices/system/cpu/cpufreq/interactive/max_freq_hysteresis 100000' -i init.mako.rc
 sed '/interactive\/max_freq_hysteresis 100000/ a\    write /sys/devices/system/cpu/cpufreq/interactive/timer_slack 80000' -i init.mako.rc
@@ -49,8 +49,8 @@ sed '/kgsl-3d0\/max_gpuclk 200000000/ a\    # Cpu-Boost' -i init.mako.rc
 sed '/kgsl-3d0\/max_gpuclk 200000000/ a\\' -i init.mako.rc
 
 sed '/# Cpu-Boost/ a\    write /sys/module/cpu_boost/parameters/boost_ms 20' -i init.mako.rc
-sed '/cpu_boost\/parameters\/boost_ms 20/ a\    write /sys/module/cpu_boost/parameters/input_boost_freq 918000' -i init.mako.rc
-sed '/cpu_boost\/parameters\/input_boost_freq 918000/ a\    write /sys/module/cpu_boost/parameters/input_boost_ms 100' -i init.mako.rc
+sed '/cpu_boost\/parameters\/boost_ms 20/ a\    write /sys/module/cpu_boost/parameters/input_boost_freq 1026000' -i init.mako.rc
+sed '/cpu_boost\/parameters\/input_boost_freq 1026000/ a\    write /sys/module/cpu_boost/parameters/input_boost_ms 100' -i init.mako.rc
 sed '/cpu_boost\/parameters\/input_boost_ms 100/ a\    # I/O' -i init.mako.rc
 sed '/cpu_boost\/parameters\/input_boost_ms 100/ a\\' -i init.mako.rc
 
